a8e08eaf1c3b1bd12087751b6c52d1e20f5b79ca
NeeXuuS <lissy.sylvain@gmail.com>
2020-09-09 17:26:57 +0200

    Fin index // Debut produit

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
+//Recuperation des données
 fetch("http://localhost:3000/api/teddies")
-    .then(reponse => reponse.json())
-    .then(reponse => {
-        console.log (reponse)
-        const main = document.getElementById("teddies_list")
-        reponse.forEach(element => {
-            const newdiv = document.createElement("div")
-            newdiv.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
-            const text=`            
+  //Formatage reponse au format JSON
+  .then(reponse => reponse.json())
+  // Recuperation du JSON tableau des oursons //
+  .then(reponse => {
+    // Log du tableau
+    console.log(reponse)
+    //
+    const main = document.getElementById("teddies_list")
+    reponse.forEach(element => {
+      const newdiv = document.createElement("div")
+      newdiv.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
+      const text = `            
             <div class="card mb-4 mb-lg-0 border-primary shadow">
               <img src="${element.imageUrl}" alt="${element.name}" class="card-img-top">
               <div class="card-body">
                 <h5 class="card-title">${element.name}</h5>
-                <p class="card-text">Prix : ${element.price /100}.00 €</p>
-                <a href="product.html?id=${element._id}" class="btn btn-outline-primary btn-block stretched-link">Envie d'adopter ${element.name} ?</a>
+                <p class="card-text">Prix : ${element.price / 100}.00 €</p>
+                <a href="product.html?id=${element._id}" class="btn btn-outline-primary btn-block stretched-link">Adopter ${element.name} ?</a>
               </div>
             </div>`
-          newdiv.innerHTML = text
-        main.appendChild(newdiv)    
-        });
-    })
+      newdiv.innerHTML = text
+      main.appendChild(newdiv)
+    });
+  })
